dc8e1ce5b216bcd6972152e7b6fc3ced09ada6db
Tai @ Flex <dch.tai@gmail.com>
2016-09-29 16:16:21 +0100

    move config to spawndefs

--- spawndefs.lua
+++ spawndefs.lua
@@ -1,3 +1,17 @@
+
+spawnstep.proximity = 5 -- radius range of player detection, around trap node
+spawnstep.spawnrange = 7 -- radius range of where the mob will spawn
+
+spawnstep.interval = 1 -- how frequently to try to spawn a new mob (seconds)
+
+spawnstep.maxmobs = 4 -- maxiumum number of mobs to allow in area defined by spawn range, around the trap node
+
+spawnstep.tiles = { -- define the tiles for the trap node
+	"default_stone.png^default_mese_crystal.png"
+}
+
+-- ++++++++++++++++++++
+
 spawnstep.mobnodes = {
 	dirtmonster = {
 		node="default:dirt",
